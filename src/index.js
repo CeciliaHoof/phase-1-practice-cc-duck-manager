84fb81c27d90a9362c3b3482697e374acc22f1c8
Cecilia <ccchimento@gmail.com>
2023-12-20 18:36:01 -0700

    second deliv complete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const baseURL = 'http://localhost:3000/ducks'
 
 //DOM selections
 const duckNav = document.querySelector('#duck-nav');
+const duckName = document.querySelector('#duck-display-name');
+const duckImage = document.querySelector('#duck-display-image')
+const duckLikes = document.querySelector('#duck-display-likes')
+console.log(duckName, duckImage, duckLikes)
 
 //fetch functions
 function getDucks(url){
@@ -16,9 +20,16 @@ function getDucks(url){
 
 //render functions
 function renderNav(duckObj){
-    const duckImg = document.createElement('img');
-    duckImg.src = duckObj.img_url;
-    duckNav.appendChild(duckImg);
+    const duck = document.createElement('img');
+    duck.src = duckObj.img_url;
+    duck.addEventListener('click', () => renderDuckDetail(duckObj))
+    duckNav.appendChild(duck);
+}
+
+function renderDuckDetail(duckObj){
+    duckName.textContent = duckObj.name;
+    duckImage.src = duckObj.img_url;
+    duckLikes.textContent = `${duckObj.likes} likes`
 }
 
 //event listeners and handlers
@@ -29,4 +40,7 @@ getDucks(baseURL)
 /*Instructions:
 1) When the page loads, fetch the ducks and display each duck image
 in the #duck-nav.
-    render duck images to #duck-nav*/
+    render duck images to #duck-nav
+2) When a user clicks one of the duck images:
+    it shows the duck's name, the image, and a likes button
+    with the number of likes in the #duck-displaylike so:*/
