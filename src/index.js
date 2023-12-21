1b1bdf58b5baf4fe84af4cddef168bf63f62441d
Cecilia <ccchimento@gmail.com>
2023-12-20 18:24:22 -0700

    first deliv complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 // write your code here!
+//global variables
+const baseURL = 'http://localhost:3000/ducks'
+
+//DOM selections
+const duckNav = document.querySelector('#duck-nav');
+
+//fetch functions
+function getDucks(url){
+    fetch(url)
+        .then(resp => resp.json())
+        .then(ducksArr => {
+            ducksArr.forEach(duckObj => renderNav(duckObj))
+        })
+    }
+
+//render functions
+function renderNav(duckObj){
+    const duckImg = document.createElement('img');
+    duckImg.src = duckObj.img_url;
+    duckNav.appendChild(duckImg);
+}
+
+//event listeners and handlers
+
+//initializers
+getDucks(baseURL)
+
+/*Instructions:
+1) When the page loads, fetch the ducks and display each duck image
+in the #duck-nav.
+    render duck images to #duck-nav*/
